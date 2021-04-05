f550a093b87b9ab91bb29f8ce5ce521e11e5417c
composed8834 <82000025+composed8834@users.noreply.github.com>
2021-04-06 03:52:13 +0500

    Delete php_blog/www directory

--- php_blog/www/admin/blocks/header.php
+++
@@ -1,3 +0,0 @@
-<tr>
-<td colspan="2" class="header"><p align="center">Добро пожаловать в админский блок</p></td>
-</tr>
--- php_blog/www/blocks/footer.php
+++
@@ -1,3 +0,0 @@
-<tr>
-<td class="footer" align="center">Дата создания сайта 2012 год</td>
-</tr>
